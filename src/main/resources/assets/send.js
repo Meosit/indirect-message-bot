c061a68a89e6fc2d68947ffef26d8dffdaa881c0
Mikhail Snitavets <mksn13@gmail.com>
2021-09-15 20:19:29 +0200

    Add image attaching through clipboard and drag-and-drop

--- src/main/resources/assets/send.js
+++ src/main/resources/assets/send.js
@@ -24,15 +24,72 @@ $(function() {
   }
 
   var messageAttachment = null;
+
+  function validateAttachment() {
+    if(messageAttachment != null) {
+      if (messageAttachment.size > (10 * 1024 * 1024)) {
+        showAlert("Attachment size must be less than 10 Megabytes", false);
+        messageAttachment = null;
+        $('#attachment-name').text('')
+      }
+    }
+  }
+
   $('#message-attachment').on('change', function(e) {
-    if (e.target.files.length === 1) {
+    if (e.target.files.length >= 1) {
       messageAttachment = e.target.files[0]
       $('#attachment-name').text(messageAttachment.name)
+      validateAttachment()
     } else {
       messageAttachment = null
       $('#attachment-name').text("")
     }
   });
+
+  $(window).on("paste", function(event){
+    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    for (index in items) {
+      var item = items[index];
+      if (item.kind === 'file') {
+        event.preventDefault();
+        messageAttachment = item.getAsFile();
+        $('#attachment-name').text(messageAttachment.name)
+        validateAttachment()
+        break;
+      }
+    }
+  })
+
+  $(window).on("drop", function(ev) {
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+    var dataTransfer = (ev.dataTransfer || ev.originalEvent.dataTransfer);
+    if (dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (dataTransfer.items[i].kind === 'file') {
+          var file = dataTransfer.items[i].getAsFile();
+          messageAttachment = dataTransfer.files[i]
+          $('#attachment-name').text(messageAttachment.name)
+          validateAttachment()
+          break;
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < dataTransfer.files.length; i++) {
+        messageAttachment = dataTransfer.files[i]
+        $('#attachment-name').text(messageAttachment.name)
+        validateAttachment()
+        break;
+      }
+    }
+  })
+
+  $(window).on("dragover", function(ev) {
+    ev.preventDefault();
+  })
 
   $('#send-form').on("submit", function(e) {
     e.preventDefault();
@@ -52,13 +109,9 @@ $(function() {
       formData.append("message-text", messageText);
     }
 
+    validateAttachment()
     if (messageAttachment != null) {
-      if (messageAttachment.size <= (10 * 1024 * 1024)) {
-        formData.append("attachment", messageAttachment, messageAttachment.name)
-      } else {
-        showAlert("Attachment size must be less than 10 Megabytes", false);
-        return;
-      }
+      formData.append("attachment", messageAttachment, messageAttachment.name)
     }
 
     if (typeof(Storage) !== "undefined") {
